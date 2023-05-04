b4a11f00d3f374abda8aeabc9fd266c638c2bb00
DONOVAN HERRION <DHerrion1437@slps.org>
2023-05-04 09:20:07 -0500

    notes

--- findout/Form1.Designer.vb
+++ findout/Form1.Designer.vb
@@ -23,34 +23,69 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Picwheel = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timermain = New System.Windows.Forms.Timer(Me.components)
+        Me.Btnexit = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Picwheel, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'PictureBox1
+        'Picwheel
         '
-        Me.PictureBox1.Image = Global.findout.My.Resources.Resources.og1
-        Me.PictureBox1.Location = New System.Drawing.Point(233, 76)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(300, 300)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
+        Me.Picwheel.Image = Global.findout.My.Resources.Resources.og1
+        Me.Picwheel.Location = New System.Drawing.Point(339, 109)
+        Me.Picwheel.Name = "Picwheel"
+        Me.Picwheel.Size = New System.Drawing.Size(425, 400)
+        Me.Picwheel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Picwheel.TabIndex = 0
+        Me.Picwheel.TabStop = False
+        Me.Picwheel.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(935, 291)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 54)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Spin"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timermain
+        '
+        Me.Timermain.Interval = 10
+        '
+        'Btnexit
+        '
+        Me.Btnexit.Location = New System.Drawing.Point(1080, 21)
+        Me.Btnexit.Name = "Btnexit"
+        Me.Btnexit.Size = New System.Drawing.Size(116, 38)
+        Me.Btnexit.TabIndex = 3
+        Me.Btnexit.Text = "Exit"
+        Me.Btnexit.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1208, 702)
+        Me.Controls.Add(Me.Btnexit)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Picwheel)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picwheel, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Picwheel As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timermain As Timer
+    Friend WithEvents Btnexit As Button
+    Friend WithEvents Timer2 As Timer
 End Class
--- findout/Form1.vb
+++ findout/Form1.vb
@@ -1,3 +1,95 @@
-﻿Public Class Form1
+﻿Imports System.Diagnostics.Eventing.Reader
 
+Public Class Form1
+    Dim number As Integer
+    Dim random As Decimal
+    Dim number2 As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Picwheel.Visible = True
+        Timermain.Enabled = True
+        random = Int((Rnd() * 7 + 1))
+        Button1.Text = random
+        number = 0
+        number2 = 0
+
+    End Sub
+
+    Private Sub Timermain_Tick(sender As Object, e As EventArgs) Handles Timermain.Tick
+        'Number += 1
+        number2 += 1
+
+        If number2 = 5 Then
+            Picwheel.Image = My.Resources.og1
+        ElseIf number2 = 10 Then
+            Picwheel.Image = My.Resources._2
+        ElseIf number2 = 15 Then
+            Picwheel.Image = My.Resources._3
+        ElseIf number2 = 20 Then
+            Picwheel.Image = My.Resources._4
+        ElseIf number2 = 25 Then
+            Picwheel.Image = My.Resources._5
+        ElseIf number2 = 30 Then
+            Picwheel.Image = My.Resources._6
+        ElseIf number2 = 35 Then
+            Picwheel.Image = My.Resources._7
+        ElseIf number2 = 40 Then
+            Picwheel.Image = My.Resources._8
+        ElseIf number2 = 45 Then
+            Picwheel.Image = My.Resources._9
+        ElseIf number2 = 50 Then
+            Picwheel.Image = My.Resources._10
+        ElseIf number2 = 55 Then
+            Picwheel.Image = My.Resources._11
+        ElseIf number2 = 60 Then
+            Picwheel.Image = My.Resources._12
+        ElseIf number2 = 65 Then
+            Picwheel.Image = My.Resources._13
+        ElseIf number2 = 70 Then
+            Picwheel.Image = My.Resources._14
+        ElseIf number2 = 75 Then
+            Picwheel.Image = My.Resources._15
+        ElseIf number2 = 80 Then
+            Picwheel.Image = My.Resources._16
+        ElseIf number2 = 85 Then
+            Picwheel.Image = My.Resources._17
+        ElseIf number2 = 90 Then
+            Picwheel.Image = My.Resources._18
+        ElseIf number2 = 95 Then
+            Picwheel.Image = My.Resources._19
+        ElseIf number2 = 100 Then
+            Picwheel.Image = My.Resources._20
+        ElseIf number2 = 105 Then
+            Picwheel.Image = My.Resources._21
+        ElseIf number2 = 110 Then
+            Picwheel.Image = My.Resources._21
+        ElseIf number2 = 115 Then
+            Picwheel.Image = My.Resources._23
+        ElseIf number2 = 120 Then
+            Picwheel.Image = My.Resources._24
+        ElseIf number2 = 125 Then
+            Picwheel.Image = My.Resources._25
+            number += 1
+            If number <> 25 Then
+                number2 = 0
+            End If
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Timermain.Enabled = False
+        Button1.Text = "Spin"
+    End Sub
+
+    Private Sub Btnexit_Click(sender As Object, e As EventArgs) Handles Btnexit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+    End Sub
 End Class
